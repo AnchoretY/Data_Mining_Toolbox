e9511f9280442c5439f79a775dce11c5386b00a3
AnchoretY <752083104@qq.com>
2020-03-14 22:11:50 +0800

    setup.py upload

---
+++ data_mining_toolbox/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="data_mining_toolbox",
+    version="1.0.0",
+    description="data mining toolbox",
+    url="https://github.com/AnchoretY/Data_Mining_Toolbox",
+    author="Anchorty",
+    author_email=" ",
+    packages=["numpy,seaborn"],
+)
